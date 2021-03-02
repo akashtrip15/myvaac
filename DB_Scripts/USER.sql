d8ba22d4f7bad45777e91b715a5f436d88fbaca4
Akash Tripathi <51534948+akashtrip15@users.noreply.github.com>
2021-03-02 15:32:59 +0530

    Add files via upload

---
+++ DB_Scripts/USER.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `user` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `User_ID` varchar(45) NOT NULL,
+  `PASSWORD` varchar(45) NOT NULL,
+  `FIRST_NAME` varchar(45) NOT NULL,
+  `LAST_NAME` varchar(45) DEFAULT NULL,
+  `MOBILE` varchar(45) DEFAULT NULL,
+  `ADDRESS` varchar(200) DEFAULT NULL,
+  `WEIGHT` int DEFAULT NULL,
+  `COUNTRY` varchar(45) DEFAULT 'INDIA',
+  `EMAIL_ID` varchar(45) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
